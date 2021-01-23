f0bf5607fe62575463a2546bdf83c2eadbe9380c
le11kgorchakov <gorchakov@ymail.colm>
2021-01-23 12:09:01 -0500

    added wallaby

--- kata/prime-number.js
+++ kata/prime-number.js
@@ -1,11 +1,8 @@
 
 function isPrime(num) {
 // number 5, if 1 x 1 != 5, 1 x 2 != 5, 1 x 3 != 5, 1 x 4 != 5, 1 x 5 = 5, then its prime
-for(let i = 1; i <= num; i++){
-    if(1 * i === num){
-        console.log(i)
-    }
-  }
+return num/2;
 }
 
-isPrime(6)
+console.log(isPrime(6));
+
